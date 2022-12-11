41d4b10eb5910d6cecb4edce261d81d3d8d7925b
Tdailey990 <bhecht2@villanova.edu>
2022-12-11 18:48:12 -0500

    Adding Mobile Functionality
    Added platform detection and conditionals for behavior on mobile vs web

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,6 +2,9 @@ import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Capacitor } from '@capacitor/core';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +13,11 @@ import { Injectable } from '@angular/core';
 export class PhotoService {
   public photos: UserPhoto[] = [];
   private PHOTO_STORAGE: string = 'photos';
+  private platform: Platform;
 
-  constructor() { }
+  constructor(platform: Platform) {
+    this.platform = platform;
+  }
 
   public async addNewToGallery() {
     const capturedPhoto = await Camera.getPhoto({
@@ -32,40 +38,58 @@ export class PhotoService {
   }
 
   public async loadSaved() {
+    // Retrieve cached photo array data
     const photoList = await Preferences.get({ key: this.PHOTO_STORAGE });
     this.photos = JSON.parse(photoList.value!) || [];
-
-    for (let photo of this.photos) {
-      const readFile = await Filesystem.readFile({
-        path: photo.filepath,
-        directory: Directory.Data,
-      });
-
-      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+  
+    if (!this.platform.is('hybrid')) {
+      for (let photo of this.photos) {
+        const readFile = await Filesystem.readFile({
+            path: photo.filepath,
+            directory: Directory.Data
+        });
+  
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+      }
     }
   }
 
+  // saves picture to file on device
   private async savePicture(photo: Photo) {
     const base64Data = await this.readAsBase64(photo);
-  
     const fileName = new Date().getTime() + '.jpeg';
     const savedFile = await Filesystem.writeFile({
       path: fileName,
       data: base64Data,
       directory: Directory.Data
     });
-  
-    return {
-      filepath: fileName,
-      webviewPath: photo.webPath
-    };
+
+    if (this.platform.is('hybrid')) {
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    }
+    else {
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath
+      };
+    }
   }
 
   private async readAsBase64(photo: Photo) {
-    const response = await fetch(photo.webPath!);
-    const blob = await response.blob();
-  
-    return await this.convertBlobToBase64(blob) as string;
+    if (this.platform.is('hybrid')) {
+      const file = await Filesystem.readFile({
+        path: photo.path!
+      });
+      return file.data;
+    }
+    else {
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      return await this.convertBlobToBase64(blob) as string;
+    }
   }
   
   private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
