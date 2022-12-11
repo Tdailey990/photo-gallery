6349e94540245cfe74839bbed0be170768a39aa5
Tdailey990 <120341143+Tdailey990@users.noreply.github.com>
2022-12-11 17:32:24 -0500

    Saving to Filesystem
    Saving photos to the filesystem to display on the grid.

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -18,13 +18,44 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     });
-  
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
-    });
+
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
 
   }
+
+  private async savePicture(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+  
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+  
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
+  }
+
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+  
+    return await this.convertBlobToBase64(blob) as string;
+  }
+  
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+
 }
 
 export interface UserPhoto {
